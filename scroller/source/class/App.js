c991653fbb8d14b857ecb9de7725222e75cc7741
Chihiro SAKATOKU <csakatoku@gmail.com>
2012-01-15 19:01:23 +0900

    Added a sample to use Zynga Scroller.

---
+++ scroller/source/class/App.js
@@ -0,0 +1,13 @@
+/**
+ * @require {core.polyfill.requestAnimationFrame}
+ */
+core.Class('p.scroller.App', {
+    construct: function(settings) {
+        var element = document.getElementById("content");
+        new EasyScroller(element, {
+            scrollingX: false,
+            scrollingY: true,
+            zooming: false
+        });
+    }
+});
