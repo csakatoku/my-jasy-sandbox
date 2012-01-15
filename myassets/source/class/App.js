008143bbc817f45546dae03e37f3406ec8b54e27
Chihiro SAKATOKU <csakatoku@gmail.com>
2012-01-16 00:23:06 +0900

    Added a sample for very basic usage of @asset and core.io.Asset

---
+++ myassets/source/class/App.js
@@ -0,0 +1,21 @@
+/**
+ * @asset {myassets/*}
+ */
+core.Class('myassets.App', {
+    construct: function(settings) {
+        var images = [
+            'myassets/images/1.jpg',
+            'myassets/images/2.jpg',
+            'myassets/images/3.jpg',
+            'myassets/images/4.jpg',
+            'myassets/images/5.jpg'
+        ];
+        core.io.Asset.load(images, function(data) {
+            var content = document.getElementById("content");
+            images.forEach(function(key) {
+                //console.log(data[key]);
+                content.appendChild(data[key].node);
+            });
+        });
+    }
+});
